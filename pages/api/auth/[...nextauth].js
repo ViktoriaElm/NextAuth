c1bd4f58ce8a53ca1743c7c33ed8e983cba51304
Viktoria_Gorkina <v.gorkina.job@gmail.com>
2023-06-18 09:34:31 +0300

    added YandexProvider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import YandexProvider from "next-auth/providers/yandex";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
@@ -27,6 +28,11 @@ export const authOptions = {
           return null
         }
       }
+    }),
+
+    YandexProvider({
+      clientId: process.env.YANDEX_CLIENT_ID,
+      clientSecret: process.env.YANDEX_CLIENT_SECRET
     }),
 
     GoogleProvider({

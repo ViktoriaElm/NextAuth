b99b60ba5e1fee1ad563433e8e13f3f568120e30
Viktoria_Gorkina <v.gorkina.job@gmail.com>
2023-08-23 17:11:25 +0300

    conmmented Vacancy files

--- components/vacancies/Vacancies.js
+++ components/vacancies/Vacancies.js
@@ -1,83 +1,83 @@
-import { useEffect, useState } from "react";
-import { Fragment } from "react";
-import SearchPanel from '../SearchPanel';
-import VacancyTHead from '../headers/VacancyTHead';
+// import { useEffect, useState } from "react";
+// import { Fragment } from "react";
+// import SearchPanel from '../SearchPanel';
+// import VacancyTHead from '../headers/VacancyTHead';
 
-export default function Vacancies({handleChange, setModalAdd}) {
-    const [vacancies, setVacancies] = useState([]);
-    const [value, setValue] = useState('');
+// export default function Vacancies({handleChange, setModalAdd}) {
+//     const [vacancies, setVacancies] = useState([]);
+//     const [value, setValue] = useState('');
 
-    const api = '/api/restricted/vacancy'; // раскладываем по трем папкам - админ (есть), кандидаты, вакансии. Все в рестриктед.
+//     const api = '/api/restricted/vacancy'; // раскладываем по трем папкам - админ (есть), кандидаты, вакансии. Все в рестриктед.
 
-    const vacancieFetch = async () => {
-        try {
-            const response = await fetch(api);
-            const data = await response.json();
-            if (response.ok && !data?.error) setVacancies(data); // проверка, что респонз ок, и дата не содержит эррор
-        } catch (error) {
-            console.log(error);
-        }
-    };
+//     const vacancieFetch = async () => {
+//         try {
+//             const response = await fetch(api);
+//             const data = await response.json();
+//             if (response.ok && !data?.error) setVacancies(data); // проверка, что респонз ок, и дата не содержит эррор
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     };
 
-    useEffect(() => {
-        vacancieFetch()
-    }, [])
+//     useEffect(() => {
+//         vacancieFetch()
+//     }, [])
 
-    const sortVacancies = (coll) => {
-        let copyVacancies = vacancies.concat();
-        const sortVacancies = copyVacancies.sort((a, b) => { return a[coll] > b[coll] ? 1 : -1 });
-        setVacancies(sortVacancies)
-    }
-    console.debug('vacancies=', vacancies);
-    const filteredVacancies = Array.isArray(vacancies) ? vacancies?.filter(vacancy => {
-        return (
-            !value
-            || vacancy?.title?.toLowerCase().includes(value.toLocaleLowerCase())
-            || vacancy?.salaryFrom?.toLowerCase().includes(value.toLocaleLowerCase())
-            || vacancy?.salaryTo?.toLowerCase().includes(value.toLocaleLowerCase())
-            || vacancy?.duties?.toLowerCase().includes(value.toLocaleLowerCase())
-            || vacancy?.requirement?.toLowerCase().includes(value.toLocaleLowerCase())
-            || vacancy?.conditions?.toLowerCase().includes(value.toLocaleLowerCase())
-        );
-    }) : [];
+//     const sortVacancies = (coll) => {
+//         let copyVacancies = vacancies.concat();
+//         const sortVacancies = copyVacancies.sort((a, b) => { return a[coll] > b[coll] ? 1 : -1 });
+//         setVacancies(sortVacancies)
+//     }
+//     console.debug('vacancies=', vacancies);
+//     const filteredVacancies = Array.isArray(vacancies) ? vacancies?.filter(vacancy => {
+//         return (
+//             !value
+//             || vacancy?.title?.toLowerCase().includes(value.toLocaleLowerCase())
+//             || vacancy?.salaryFrom?.toLowerCase().includes(value.toLocaleLowerCase())
+//             || vacancy?.salaryTo?.toLowerCase().includes(value.toLocaleLowerCase())
+//             || vacancy?.duties?.toLowerCase().includes(value.toLocaleLowerCase())
+//             || vacancy?.requirement?.toLowerCase().includes(value.toLocaleLowerCase())
+//             || vacancy?.conditions?.toLowerCase().includes(value.toLocaleLowerCase())
+//         );
+//     }) : [];
 
 
 
-    return (<>
-        <div class="main-top">
-            <SearchPanel handleChange={handleChange} setModalAdd={setModalAdd} />
+//     return (<>
+//         <div class="main-top">
+//             <SearchPanel handleChange={handleChange} setModalAdd={setModalAdd} />
            
-        </div>
-        <div class="div-table">
-            <table>
-                <VacancyTHead sortVacancies={sortVacancies} />
+//         </div>
+//         <div class="div-table">
+//             <table>
+//                 <VacancyTHead sortVacancies={sortVacancies} />
 
-                {filteredVacancies && filteredVacancies.map((vacancy) => (
+//                 {filteredVacancies && filteredVacancies.map((vacancy) => (
 
-                    <Fragment key={vacancy.id}>
-                        <tr key={vacancy.id} onClick={() => { setModalActive(true) }}>
-                                <td onClick={() => vacancies.map(vacancy)} className="td-base">
-                                    {vacancy && vacancy.title}
-                                </td>
-                                <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
-                                    { vacancy && vacancy.salaryFrom}
-                                </td>
-                                <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
-                                    {vacancy && vacancy.salaryTo}
-                                </td>
-                                <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
-                                    {vacancy && vacancy.duties}
-                                </td>
-                                <td onClick={() => setSelectedVacancy(vacancy)} className="status td-base">
-                                {vacancy && vacancy.requirement}
-                                </td>
-                                <td onClick={() => setSelectedVacancy(vacancy)} className="comment td-base">
-                                    {vacancy && vacancy.conditions}
-                                </td>
-                            </tr>
-                    </Fragment>
-                ))}
-            </table>
-        </div>
-    </>)
-}
+//                     <Fragment key={vacancy.id}>
+//                         <tr key={vacancy.id} onClick={() => { setModalActive(true) }}>
+//                                 <td onClick={() => vacancies.map(vacancy)} className="td-base">
+//                                     {vacancy && vacancy.title}
+//                                 </td>
+//                                 <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
+//                                     { vacancy && vacancy.salaryFrom}
+//                                 </td>
+//                                 <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
+//                                     {vacancy && vacancy.salaryTo}
+//                                 </td>
+//                                 <td onClick={() => setSelectedVacancy(vacancy)} className="td-base">
+//                                     {vacancy && vacancy.duties}
+//                                 </td>
+//                                 <td onClick={() => setSelectedVacancy(vacancy)} className="status td-base">
+//                                 {vacancy && vacancy.requirement}
+//                                 </td>
+//                                 <td onClick={() => setSelectedVacancy(vacancy)} className="comment td-base">
+//                                     {vacancy && vacancy.conditions}
+//                                 </td>
+//                             </tr>
+//                     </Fragment>
+//                 ))}
+//             </table>
+//         </div>
+//     </>)
+// }

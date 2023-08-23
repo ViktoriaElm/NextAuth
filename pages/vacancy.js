b219add8ab1fc86b6ff8fdbd774696f7195385c5
Viktoria_Gorkina <v.gorkina.job@gmail.com>
2023-08-23 16:58:58 +0300

    commented vacancy.js

--- pages/vacancy.js
+++ pages/vacancy.js
@@ -1,11 +1,11 @@
-import Vacancies from "../components/vacancies/Vacancies";
+// import Vacancies from "../components/vacancies/Vacancies";
 
-export default function Vacancy() {
-    return(<>
-    <div className='admin-container'>
-        <table className='admin-table'>
-        <Vacancies/>
-        </table>
-    </div>
-    </>);
-}
+// export default function Vacancy() {
+//     return(<>
+//     <div className='admin-container'>
+//         <table className='admin-table'>
+//         <Vacancies/>
+//         </table>
+//     </div>
+//     </>);
+// }

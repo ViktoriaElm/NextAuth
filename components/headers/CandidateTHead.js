a4f4cb6f1411b3c9b682ff9ca7c8f1ba5966292b
Viktoria_Gorkina <v.gorkina.job@gmail.com>
2023-08-25 08:10:27 +0300

    Sortable fields marked

--- components/headers/CandidateTHead.js
+++ components/headers/CandidateTHead.js
@@ -2,11 +2,11 @@ export default function CandidateTHead({sortCandidates}) {
     return(<>
     <thead className='admin-table'>
                     <tr className='admin-table'>
-                        <th onClick={_ => { sortCandidates('lastName') }}>ФИО</th>
-                        <th>Вакансия</th>
-                        <th onClick={sortCandidates}>Город</th>
-                        <th onClick={_ => { sortCandidates('phoneNumber') }}>Телефон</th>
-                        <th onClick={_ => { sortCandidates('email') }}>E-mail</th>
+                        <th onClick={_ => { sortCandidates('lastName') }}>ФИО ◦</th>
+                        <th onClick={_ => { sortCandidates('vacancy') }}>Вакансия ◦</th>
+                        <th onClick={_ => { sortCandidates('address') }}>Город ◦</th>
+                        <th>Телефон</th>
+                        <th>E-mail</th>
                         <th>Статус</th>
                         <th>Комментарий</th>
                     </tr>
